a13a59aba1d4fe451cb0bb72664d76837b52c251
CebotariTigran <cebotaritigran@gmail.com>
2023-07-12 13:23:12 -0400

    Add get all loads function

--- src/loads.js
+++ src/loads.js
@@ -4,5 +4,5 @@ topBody.appendChild(getAllLoadsButton)
 getAllLoadsButton.innerText = 'Get all loads';
 
 getAllLoadsButton.addEventListener("click", () => {
-    console.log(document.querySelectorAll('.css-ly5121'));
+    console.log(document.querySelectorAll('.css-ly5121 .css-5wftrk'));
 })
