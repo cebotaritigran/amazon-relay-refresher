9ec2258a9f2bc7708912da65308fba738d103d59
CebotariTigran <cebotaritigran@gmail.com>
2023-07-19 11:36:18 -0400

    Grab token from header but still can't use it amk

--- src/serviceWorker/service_worker.js
+++ src/serviceWorker/service_worker.js
@@ -1,6 +1,12 @@
 var requestFilter = { urls: ["https://relay.amazon.com/*"] };
 var extraInfoSpec = ['requestHeaders'];
+let token;
 var handler = function (details) {
+    if (details.method = "POST") {
+        token = details["requestHeaders"][2]
+        token = token["value"]
+        console.log(token)
+    }
     console.log(details);
 };
 
